ae6493e2a9feaa304c029b2c6d0ba16126a87fbe
unknown <dimas302putra@gmail.com>
2023-08-08 09:01:11 +0700

    first commit

---
+++ class/class_perhitungan.php
@@ -0,0 +1,28 @@
+<?php 
+
+class perhitungan{
+
+    var $bil1, $bil2, $bil3;
+
+    function luasPersegi($bil1){
+        $sisi=$bil1;
+        $luas=$sisi*$sisi;
+        return "sisi =" .$sisi. "<br> Luas =" .$sisi." x ".$sisi." <br> Hasil = ".$luas;
+    }
+
+    function LuasPersegiPanjang($bil1,$bil2){
+        $panjang=$bil1;
+        $lebar=$bil2;
+        $luas=$panjang*$lebar;
+        return "Panjang =" .$panjang."<br> Lebar =" .$lebar."<br> Luas =".$panjang." x ".$lebar." <br> Hasil =".$luas;
+    }
+
+    function LuasSegitiga($bil1,$bil2){
+        $alas=$bil1;
+        $tinggi=$bil2;
+        $luas=$alas*$tinggi/2;
+        return "Alas =" .$alas."<br> Tinggi =" .$tinggi."<br> Luas =".$alas." x ".$tinggi.":2 <br> Hasil =".$luas;    
+    }
+    
+}
+?>
